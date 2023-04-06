75a2c7fe960689134fe658342055bb4df937daa4
haariskhatri <haarishkkhatri@gmail.com>
2023-04-06 20:17:17 +0530

    fs added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const fs = require("fs");
 const fetch = require("node-fetch");
 const _ = require("lodash");
 
