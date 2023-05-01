e502174e70501a784be7112021aa1a1f0975f193
Haaris Khatri <57015634+haariskhatri@users.noreply.github.com>
2023-05-01 23:49:32 +0530

    updated ssl certificate

--- app.js
+++ app.js
@@ -260,11 +260,11 @@ async function check(data) {
   return result;
 }
 
-// const options = {
-//   key: fs.readFileSync('/etc/letsencrypt/live/app.ajrakhhouse.com/privkey.pem'),
-//   cert: fs.readFileSync('/etc/letsencrypt/live/app.ajrakhhouse.com/fullchain.pem')
-// }
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/app.ajrakhhouse.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/app.ajrakhhouse.com/fullchain.pem')
+}
 
- //https.createServer(options, app).listen(4000, console.log(`server runs on port 4000`))
+https.createServer(options, app).listen(4000, console.log(`server runs on port 4000`))
 
-app.listen(5000);
+//app.listen(5000);
